d1933faf85afc4b62c9f78a8b15a03eee79adb72
prokhorovxo <prokhorovxo@yahoo.com>
2023-01-06 00:12:56 +0300

    Initial Commit

---
+++ ExampleApolloGraphQL/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ExampleApolloGraphQL
+//
+//  Created by Fedor Prokhorov on 06.01.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
