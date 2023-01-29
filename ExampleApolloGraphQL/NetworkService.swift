5478c5ffdc75026b4836fd7b1f290614b2725a0d
prokhorovxo <prokhorovxo@yahoo.com>
2023-01-29 22:17:58 +0300

    Update API URL

--- ExampleApolloGraphQL/NetworkService.swift
+++ ExampleApolloGraphQL/NetworkService.swift
@@ -12,7 +12,7 @@ final class NetworkService {
     
     static let shared = NetworkService()
     
-    private(set) var apollo = ApolloClient(url: URL(string: "https://api.spacex.land/graphql/")!)
+    private(set) var apollo = ApolloClient(url: URL(string: "https://spacex-production.up.railway.app/")!)
     
     private init() { }
 }
